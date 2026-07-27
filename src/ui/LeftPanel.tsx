6b080651ca6d6b886b03317fc9f23ee6de57cad8
Amalia <amaliantsha@gmail.com>
2026-07-27 20:22:37 +0800

    type shit

--- src/ui/LeftPanel.tsx
+++ src/ui/LeftPanel.tsx
@@ -1,9 +1,12 @@
 export function LeftPanel() {
     return (
-        <div className="bg-[var(--red-500)] py-10 px-12 w-full text-white">
+        <div className=" py-10 px-12 w-full text-white">
             {/* dummy content — build on this later */}
-            <div className="heading text-red-400 font-hiker leading-none text-[clamp(3rem,12vw,10rem)]">
-                Left Panel
+            <div className="heading text-red-400 font-timegoing leading-none text-[clamp(3rem,12vw,10rem)]">
+                SKILLz
+            </div>
+            <div className="flex">
+                <img src="images/right-arrow.png" alt="Right arrow" />
             </div>
         </div>
     )
